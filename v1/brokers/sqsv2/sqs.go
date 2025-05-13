f3b35d6c4c7427de8d17eacdcf4ac3e9bc0e6fc3
Syed Abeer Hussain <syed.hussain@securiti.ai>
2025-05-13 15:19:27 +0500

    Improve error logging for message sending by distinguishing queue existence errors.

--- v1/brokers/sqsv2/sqs.go
+++ v1/brokers/sqsv2/sqs.go
@@ -20,6 +20,7 @@ import (
 	awscfg "github.com/aws/aws-sdk-go-v2/config"
 	awssqs "github.com/aws/aws-sdk-go-v2/service/sqs"
 	"github.com/aws/aws-sdk-go-v2/service/sqs/types"
+	"github.com/aws/aws-sdk-go/service/sqs"
 )
 
 const (
@@ -193,7 +194,11 @@ func (b *Broker) Publish(ctx context.Context, signature *tasks.Signature) error 
 	result, err := b.service.SendMessage(ctx, MsgInput)
 
 	if err != nil {
-		log.ERROR.Printf("Error when sending a message: %v", err)
+		if strings.Contains(err.Error(), sqs.ErrCodeQueueDoesNotExist) {
+			log.INFO.Printf("Error Queue doesn't exist: %v", err)
+		} else {
+			log.ERROR.Printf("Error when sending a message: %v", err)
+		}
 		return err
 
 	}
